2ef22f9cf712363c2423b662c19965ed64c9593c
ffekirnew <ffekirnew0808@gmail.com>
2023-08-08 19:43:06 +0300

    Activate Swagger documentation

--- week-2/day-7/BlogApp/Program.cs
+++ week-2/day-7/BlogApp/Program.cs
@@ -23,8 +23,8 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddScoped<CommentApplication>();
 
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    // builder.Services.AddEndpointsApiExplorer();
-    // builder.Services.AddSwaggerGen();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 }
 
 var app = builder.Build();
@@ -32,11 +32,11 @@ var app = builder.Build();
 
 {
     // Configure the HTTP request pipeline.
-    // if (app.Environment.IsDevelopment())
-    // {
-    //     app.UseSwagger();
-    //     app.UseSwaggerUI();
-    // }
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
     app.UseHttpsRedirection();
 
