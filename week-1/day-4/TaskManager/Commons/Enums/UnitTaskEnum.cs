0e24bc42da22f28ff1092a215b63e28edb04c96a
ffekirnew <ffekirnew0808@gmail.com>
2024-03-12 14:13:17 +0300

    feat: Work on Simple Task Manager project

--- week-1/day-4/TaskManager/Commons/Enums/UnitTaskEnum.cs
+++ week-1/day-4/TaskManager/Commons/Enums/UnitTaskEnum.cs
@@ -2,10 +2,10 @@ namespace TaskManager.Commons.Enums;
 
 enum UnitTaskEnum
 {
-    None = 0,
-    Personal = 1,
-    Work = 2,
-    Errands = 4,
-    Others = 8,
-    All = 15
+  None = 0,
+  Personal = 1,
+  Work = 2,
+  Errands = 4,
+  Others = 8,
+  All = 15
 }
--- week-1/day-4/TaskManager/UnitTask.cs
+++ week-1/day-4/TaskManager/UnitTask.cs
@@ -3,16 +3,16 @@ namespace TaskManager;
 
 class UnitTask
 {
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public UnitTaskEnum Category { get; init; }
-    public bool IsCompleted { get; set; }
+  public string? Name { get; set; }
+  public string? Description { get; set; }
+  public UnitTaskEnum Category { get; init; }
+  public bool IsCompleted { get; set; }
 
-    public UnitTask()
-    {
-    }
+  public UnitTask()
+  { }
 
-    public override string ToString() => $"Name: ({Name}) Description: ({Description}) Category: ({Category}) IsCompleted: ({IsCompleted})";
-    public string ToCSV() => $"{Name},{Description},{(int) Category},{IsCompleted}";
+  public override string ToString() => $"Name: ({Name}) Description: ({Description}) Category: ({Category}) IsCompleted: ({IsCompleted})";
+
+  public string ToCSV() => $"{Name},{Description},{(int)Category},{IsCompleted}";
 
 }
